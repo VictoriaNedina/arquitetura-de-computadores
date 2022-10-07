2d57dce78e474dccb2ed5af10fcb3583d6361ba9
Victoria Nedina Nunes <victoria-nedina@hotmail.com>
2022-10-06 23:13:03 -0300

    feat: minimum program

--- hello.asm
+++ hello.asm
@@ -1,14 +1,17 @@
+; Hello World -> Mínimo Programa
+; Compilar = transformar o programa em linguagem de máquina
+;   nasm -f elf64 <archive_name>.asm
+; Linkeditar = transformar o programa de linguagem máquina para executável
+;   ld -s -o <archive_name> <archive_name>.o
 section .data
-;Hello World 
+
 section .text 
 
 global _start
 
 _start:
-    ;destino -> origem EAX = EAX + 1
+    ; destino -> origem
     ; 0 1 2 3 4 5 6 7 8 9 A B C D E F (hexadecimal)
-    mov EAX, 0x1 ; SO terminando programa
+    mov EAX, 0x1 ; SO terminando
     mov EBX, 0x0 ; SO valor retorno é 0
-    int 0x80 ; SO câmbio desligo
-    
-    ; p/ compilar = nasm -f elf64 <archive_name>.asm
+    int 0x80 ; SO câmbio
